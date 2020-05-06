da0f39debfe30ff39a4a8437f268782bb31d5b30
Bencece <zahoranbence@hotmail.com>
2020-05-06 20:13:45 +0200

    swap works

---
+++ script.js
@@ -0,0 +1,74 @@
+let N = 10;
+let blockSize = 500 / N;
+let gameArea;
+
+var green = '<img src="./img/green.png" class="candy green" />';
+var green_s = '<img src="./img/green_s.png" class="candy green_s" />';
+var orange = '<img src="./img/orange.png" class="candy orange" />';
+var orange_s = '<img src="./img/orange_s.png" class="candy orange" />';
+var purple = '<img src="./img/purple.png" class="candy purple" />';
+var purple_s = '<img src="./img/purple_s.png" class="candy purple_s" />';
+var red = '<img src="./img/red.png" class="candy red" />';
+var red_s = '<img src="./img/red_s.png" class="candy red_s" />';
+var yellow = '<img src="./img/yellow.png" class="candy yellow" />';
+var yellow_s = '<img src="./img/yellow_s.png" class="candy yellow_s" />';
+
+var blocks = []
+var swap;
+
+function generateMap(){
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N; j++) {
+            let block = $('<div></div>');
+            block.addClass('block');
+            let random = Math.random();
+            if (0 <= random && random < 0.20) {
+                block.append(green)
+            } else if (0.20 <= random && random < 0.40) {
+                block.append(orange)
+            } else if (0.40 <= random && random < 0.60) {
+                block.append(purple)
+            } else if (0.60 <= random && random < 0.80) {
+                block.append(red)
+            } else {
+                block.append(yellow)
+            }
+            block.css({
+                width: blockSize,
+                height: blockSize,
+                top: i * blockSize,
+                left: j * blockSize
+            });
+            block.on("click", () =>{
+                highLight(block)
+            })
+            block.appendTo(gameArea);
+        }
+    }
+}
+
+function highLight(block){
+    if(swap == null){
+        swap=block;
+    } else {
+        var top = block.css("top")
+        var left = block.css("left")
+        block.animate({
+            "top" : swap.css("top"),
+            "left" : swap.css("left")
+        },300)
+        swap.animate({
+            "top" : top,
+            "left" : left
+        },300)
+        swap=null
+    }
+}
+
+$(function () {
+    gameArea = $('<div></div>');
+    gameArea.appendTo('body');
+    gameArea.attr('id', 'gamearea');
+    
+    generateMap();
+});
